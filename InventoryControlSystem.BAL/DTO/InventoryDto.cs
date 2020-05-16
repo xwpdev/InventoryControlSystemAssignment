a8f9faa55a1e27a2506c1cb457bd3da812ac195d
xwpdev <charith.suriyakula@gmail.com>
2020-05-16 18:40:16 +0530

    api code cleanup

--- InventoryControlSystem.BAL/DTO/InventoryDto.cs
+++ InventoryControlSystem.BAL/DTO/InventoryDto.cs
@@ -9,27 +9,16 @@ namespace InventoryControlSystem.BAL.DTO
     public class InventoryDto
     {
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public int UnitCount { get; set; }
-
         public decimal UnitPrice { get; set; }
-
         public int ReorderCount { get; set; }
-
         public string AddedBy { get; set; }
-
         public string AddedByName { get; set; }
-
         public DateTime AddedDate { get; set; }
-
         public string UpdatedBy { get; set; }
-
         public string UpdatedByName { get; set; }
-
         public DateTime UpdatedDate { get; set; }
     }
 }
--- InventoryControlSystem.DAL/Models/Inventory.cs
+++ InventoryControlSystem.DAL/Models/Inventory.cs
@@ -8,28 +8,17 @@ namespace InventoryControlSystem.DAL.Models
     {
         [Key]
         public int Id { get; set; }
-
         public string Name { get; set; }
-        
         public string Description { get; set; }
-
         public int UnitCount { get; set; }
-        
         public decimal UnitPrice { get; set; }
-
         public int ReorderCount { get; set; }
-
         public string AddedBy { get; set; }
-
         public string AddedByName { get; set; }
-        
         [Column(TypeName = "datetime2")]
         public DateTime AddedDate { get; set; }
-
         public string UpdatedBy { get; set; }
-
         public string UpdatedName { get; set; }
-
         [Column(TypeName = "datetime2")]
         public DateTime UpdatedDate { get; set; }
     }
